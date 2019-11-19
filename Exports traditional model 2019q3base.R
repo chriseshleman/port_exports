c1d0bb31a98624703ba4ede92b150740e80bc501
chriseshleman <eshlemanchris@gmail.com>
2019-11-19 17:14:15 -0500

    third commit

--- Exports traditional model 2019q3base.R
+++ Exports traditional model 2019q3base.R
@@ -14,6 +14,7 @@ library(car)
 library(dplyr) 
 library(DataCombine)
 library(glmnet) 
+library(Hmisc) 
 
 
 setwd("~/Dropbox/Work and research/Port Authority/Port Exports 2019/port_exports")
@@ -22,17 +23,11 @@ setwd("~/Dropbox/Work and research/Port Authority/Port Exports 2019/port_exports
 # TRADITIONAL MODEL TRADITIONAL MODEL TRADITIONAL MODEL TRADITIONAL MODEL 
 ####################################################################################
 
-
 exports = read.csv("./Loaded_exports19Q3.csv") 
-exports$quarter = seq(as.Date("1997/1/1"), as.Date("2019/04/01"), by="quarter") # Add date
+exports$quarter = seq(as.Date("1997/1/1"), as.Date("2019/07/01"), by="quarter") # Add date
 
-tail(exports) 
 exports$Year=NULL 
 exports$Quarter=NULL 
-#dat3 = merge(exports, dat2, by = "quarter", all.x=TRUE) 
-#dat3$Year=NULL 
-#head(exports) 
-#summary(as.numeric(dat3$Japan..Real.Price.of.Crude.Oil..Units..real.2011.Japanese.yen.per.barrel..Data.source..IHS.Economics.calculation.using.data.from.Department.of.Energy)) 
 
 # Add time series 
 old = read.csv("./Inputs_19Q2pess.csv") 
@@ -40,8 +35,8 @@ old$quarter = seq(as.Date("1997/1/1"), as.Date("2037/10/01"), by="quarter") # Ad
 old$Mnemonic = NULL 
 exports_old = merge(exports,old,by="quarter", all.y=TRUE) 
 
-hist = subset(exports_old,exports_old$quarter<="2019-04-01") 
-forc = subset(exports_old,exports_old$quarter>"2019-04-01") 
+hist = subset(exports_old,exports_old$quarter<="2019-07-01") 
+forc = subset(exports_old,exports_old$quarter>"2019-07-01") 
 
 hist_reg = hist 
 hist_reg$Actual=NULL 
@@ -82,19 +77,20 @@ export_predict
 #write.csv(summary(fit),"./Equation from arima_base 20190903.csv") 
 
 
-# 
+ 
 fit_alt = arima(ts(hist$Actual), xreg = hist_reg, order=c(0,0,0), include.mean=T) # as of 2018-09 (1,0,1) before that (1,1,2)) 
 
-export_predict = predict(fit_alt, n.ahead=75, newxreg=forc_reg, level=95) #interval = "prediction", conf.level=.9) # predict 
-export_predict$quarter = seq(as.Date("2019-04-01"),as.Date("2037-10-01"),by="quarter")  
+export_predict.alt = predict(fit_alt, n.ahead=74, newxreg=forc_reg, level=95) #interval = "prediction", conf.level=.9) # predict 
+export_predict.alt$quarter = seq(as.Date("2019-07-01"),as.Date("2037-10-01"),by="quarter")  
 
 summary(fit_alt) 
-export_predict 
+export_predict.alt
 resids_alt = as.vector(resid(fit_alt)) 
 durbinWatsonTest(resids_alt)
 toy.fit_alt = as.data.frame(cbind(hist$Actual,fitted(fit_alt))); names(toy.fit_alt) = c("actual","fitted")
 mape_alt = mean(abs(toy.fit_alt$actual-toy.fit_alt$fitted/toy.fit_alt$actual) * 100) # MAPE # rowMeans
 rm(toy.fit_alt) 
+
 #d = sum((fit_alt$residuals - lag(fit_alt$residuals))^2, na.rm = TRUE) /
 # sum(fit_alt$residuals^2, na.rm = TRUE) 
 
@@ -118,37 +114,40 @@ other = other %>% select_if(not_all_na) # Remove variables with all NAs.
 # (There aren't any.) 
 other.noNa = other %>%
   select_if(~ !any(is.na(.)))  # Remove variables with any NAs. 
+exports.mega = merge(exports_old, other.noNa, by="quarter") 
 
-exports_old.mega = merge(exports_old, other.noNa, by="quarter") 
+####### 
 
 # Lag the variable 
-actual_lag = slide(exports_old.mega, Var = "Actual", slideBy = -1)
-exports_old.mega = cbind(exports_old.mega,actual_lag) 
-exports_old.mega = exports_old.mega[-1,] # Dump the first row 
+# exports.mega = slide(exports.mega, Var = "Actual", slideBy = -1) # This way sucks 
+exports.mega$Actual_1 = Lag(exports.mega$Actual, +1) # Way better. 
 
-hist.mega = subset(exports_old.mega,exports_old.mega$quarter<="2019-04-01") 
-forc.mega = subset(exports_old.mega,exports_old.mega$quarter>"2019-04-01") 
+exports.mega = exports.mega[-1,] # Dump the first row 
 
-# Elastic Net 
-blend = .9 # pick penalization process (between 0 and 1)
-hist.mega.actual = hist.mega[,2] # Distinguish second column, the Y variable
+hist.mega = subset(exports.mega,exports.mega$quarter<="2019-07-01") # Take the old stuff for building model 
+
+# Prep for regularization 
+# Split into Y and everything else, and dump quarter variable
+actual = hist.mega[,2] # Distinguish second column, the Y variable 
+
 #hist.mega.matrix = hist.mega[,-1:2] # Lose the first and second column in main matrix 
 hist.mega.matrix = hist.mega[,-1] # Lose the first and second column in main matrix 
-hist.mega.matrix = hist.mega.matrix[,-1] # Not sure why this takes two rows but it does. 
+hist.mega.matrix = hist.mega.matrix[,-1] 
 
 hist.mega.matrix = data.matrix(hist.mega.matrix) 
 
-set.seed(1042) 
-cv.mega = cv.glmnet(hist.mega.matrix, hist.mega.actual, alpha = blend, nfolds = 10) 
+# Elastic Net 
+set.seed(1039) 
+cv.mega = cv.glmnet(hist.mega.matrix, actual, alpha = 1, nfolds = 10) # pick penalization process (between 0 and 1) 
+options(scipen=999)
 plot(cv.mega) 
-set.seed(222)
-cv.model = glmnet(hist.mega.matrix, hist.mega.actual, alpha=blend, lambda=cv.mega$lambda.1se) 
+set.seed(221)
+cv.model = glmnet(hist.mega.matrix, actual, alpha=1, lambda=cv.mega$lambda.1se) 
 cv.model 
 coef(cv.model) 
 # So extract those coefficients 
     # https://gist.github.com/ydavidchen/a166bc364cfadd53921bb9a6f07100bb
-
-myCoefs = coef(cv.model); #, s="lambda.min");
+myCoefs = coef(cv.model); 
 myCoefs[which(myCoefs != 0 ) ]               #coefficients: intercept included
 myCoefs@Dimnames[[1]][which(myCoefs != 0 ) ] #feature names: intercept included
 
@@ -156,11 +155,63 @@ myResults = data.frame(
   features = myCoefs@Dimnames[[1]][ which(myCoefs != 0 ) ], #intercept included
   coefs    = myCoefs              [ which(myCoefs != 0 ) ]  #intercept included
 )
+
 myResults 
-head(myResults) 
-str(myResults) 
-myNames = c(myResults$features) 
-myNames 
+
+exports.mini = exports.mega[myResults$features] 
+exports.mini$Q2 = NULL 
+exports.mini$Q3 = NULL 
+exports.mini$Q4 = NULL 
+exports.dummies = exports_old[c("quarter","Q2", "Q3", "Q4")] 
+#head(exports_old[-1,1:2]) 
+#exports.mini = cbind(exports.dummies[-1,1:2],exports.mini) #exports_old$Actual[-1,])#,exports_old.mega)
+head(exports.dummies) 
+head(exports.mini) 
 
 #
-# cv.predict = predict(cv.mega, s=cv.mega$lambda.1se ,newx=hist.mega.actual)
+exports.mini = merge(exports.mini,exports.dummies, by="quarter")  
+
+# Fit ARIMA terms 
+before = subset(exports.mini,exports.mini$quarter <= "2019-07-01") 
+after = subset(exports.mini,exports.mini$quarter > "2019-07-01") 
+
+oldreg=as.matrix(data.frame(before$GDP..G8.Q.FGBA1, before$GCRE.WEHEM.Q.FGBA1, before$WPI06.Q.FMFT, 
+                             before$CR.NP.WEHEM.Q.FGBA1, before$WPI10.Q.FMFT, before$WPI09.Q.FMFT, 
+                             before$JEXCHMTPREAL.Q.FMFT, before$WPIINDO.Q.FMFT, before$Q2, before$Q3, before$Q4))#, before$Actual_1)) 
+newreg=as.matrix(data.frame(after$GDP..G8.Q.FGBA1, after$GCRE.WEHEM.Q.FGBA1, after$WPI06.Q.FMFT, 
+                             after$CR.NP.WEHEM.Q.FGBA1, after$WPI10.Q.FMFT, after$WPI09.Q.FMFT, 
+                             after$JEXCHMTPREAL.Q.FMFT, after$WPIINDO.Q.FMFT, after$Q2, after$Q3, after$Q4))#, after$Actual_1)) 
+
+# Model 
+dim(oldreg)[1]==length(before$Actual) 
+#fit.net = arima(ts(before$week),xreg = oldreg, order=c(0,0,1), include.mean=T)# as of 2018-09 (1,0,1) before that (1,1,2)) 
+fit.net = auto.arima(ts(before$Actual),xreg=oldreg, ic="aic", trace=TRUE, allowdrift=FALSE)#,lambda=0, seasonal=TRUE)# 
+
+predict.net = predict(fit.net, n.ahead=nrow(after), newxreg=newreg, level=95)#interval = "prediction", conf.level=.9) # predict 
+predict.net$quarter = seq(as.Date("2019-07-01"),as.Date("2037-10-01"),by="quarter") #+extra 
+
+
+summary(fit.net) 
+resids.net = as.vector(resid(fit.net)) 
+durbinWatsonTest(resids.net)
+# toy.fit.net = as.data.frame(cbind(actual,fitted(fit.net))); names(toy.fit.net) = c("actual","fitted")
+# mape.net = mean(abs(toy.fit.net$actual-toy.fit.net$fitted/toy.fit.net$actual) * 100) # MAPE # rowMeans
+rm(toy.fit.net) 
+### DONE 
+
+export_predict 
+
+predict.net
+
+# Diagnostics 
+out1 = tidy(fit) 
+#out2 = tidy(glance(fit)) ## why is this crashing my program? 
+out2 = glance(fit) 
+out1 
+out2 
+out3 = tidy(fitsat) 
+#out4 = tidy(glance(fitsat)) ## why is this crashing my program? 
+out5 = tidy(fitsun) 
+#out6 = tidy(glance(fitsun)) ## why is this crashing my program? 
+accuracy(fit) 
+accuracy(fit)[,'MAPE']
